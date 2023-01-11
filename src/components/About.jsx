87ad04fd6fec41fa5eb10914a613f19ddb4b93b4
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-11 21:39:54 +0200

    ADD AOS lib & ADD animations @ About comp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import { Button, Title } from "./index";
 
 import PersonImage from "../assets/images/john-doe.png";
 
 const About = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 800
+        });
+    }, []);
+
     return (
         <section className="w-screen bg-white text-black" id="about">
             <div className="container grid grid-cols-1 lg:grid-cols-2 items-center gap-y-10 lg:gap-6">
-                <div className="w-[90%] md:w-[60%] lg:w-[70%] mx-auto">
+                <div className="w-[90%] md:w-[60%] lg:w-[70%] mx-auto" data-aos="fade-right" data-aos-easing="ease-in">
                     <img src={PersonImage} alt="John Doe" className="w-full object-cover" draggable="false" />
                 </div>
 
-                <div className="w-full">
+                <div className="w-full" data-aos="fade-left" data-aos-easing="ease-in">
                     <Title>about</Title>
 
                     <div className="my-12 text-lg md:text-2xl">
@@ -21,11 +32,12 @@ const About = () => {
                         <p className="mt-8 font-semibold italic">
                             I am John Doe, a senior advisor for an independently owned financial planning company called Intense.
                         </p>
+
+                        <Button customStyle="mt-8 px-10 bg-secondary text-white">
+                            explore more
+                        </Button>
                     </div>
 
-                    <Button customStyle="px-10 bg-secondary text-white">
-                        explore more
-                    </Button>
                 </div>
             </div>
         </section>
