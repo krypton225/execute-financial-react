a0de9f8478c011003999cfe952583e3f581e4dd1
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-11 19:07:41 +0200

    ADD link scroll to Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-scroll";
+
 import LogoImage from "../assets/images/logo.png";
 
 const Logo = () => {
     return (
         <div className="w-[9rem] md:w-[13rem]">
-            <a href="#home">
+            <Link to={`home`} activeClass="active" spy={true} smooth={true} duration={1200} href="#home">
                 <img src={LogoImage} alt="Logo" className="w-full" draggable="false" />
-            </a>
+            </Link>
         </div>
     )
 }
