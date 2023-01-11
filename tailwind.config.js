f898e5ad0c46b4cf32dc35c92e23db4852992cf1
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-11 09:11:50 +0200

    ADD tailwind css & its configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F6E137"
+      },
+    },
+  },
+  plugins: [],
+}
