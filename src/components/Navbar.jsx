378eb12249af173b58803a7a77388d42487252ee
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-11 10:29:07 +0200

    ADD responsiveness to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,47 @@
 import NavItems from "../data/navItems";
 
 import LogoImage from "../assets/images/logo.png";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [toggleMenuNavbar, setToggleMenuNavbar] = useState(false);
+
+    const onClickOpenMenuHandler = () => setToggleMenuNavbar(prev => !prev);
+
     return (
-        <nav className="w-screen py-2 fixed top-0 left-0 bg-black text-white">
+        <nav className="w-screen py-2 fixed top-0 left-0 bg-black text-white z-[999]">
             <div className="container flex justify-between items-center">
-                <div className="w-[8rem] md:w-[13rem]">
+                <div className="w-[9rem] md:w-[13rem]">
                     <img src={LogoImage} alt="Logo" className="w-full" />
                 </div>
 
-                <ul className="flex items-center">
+                <ul className="hidden md:flex items-center">
                     {
                         NavItems.map(({ id, itemText, itemLink }) => (
                             <li key={id} className="ml-10 capitalize text-lg font-semibold">
-                                <a href={`#${itemLink}`} className="py-4 transition-all duration-[0.7s] hover:text-primary">{itemText}</a>
+                                <a href={`#${itemLink}`} className="block py-4 transition-all duration-[0.7s] hover:text-primary">{itemText}</a>
                             </li>
                         ))
                     }
                 </ul>
+
+                <div className={`w-[2rem] md:w-[2.3rem] h-[1.4rem] relative flex flex-col justify-between items-center md:hidden cursor-pointer`}
+                    onClick={onClickOpenMenuHandler}>
+                    <span className={`toggle-line ${toggleMenuNavbar ? "-rotate-[45deg] translate-y-3" : "rotate-[0deg] translate-y-0"}`}></span>
+                    <span className={`toggle-line ${toggleMenuNavbar ? "opacity-0 invisible translate-x-4" : "opacity-1 visible translate-x-0"}`}></span>
+                    <span className={`toggle-line ${toggleMenuNavbar ? "rotate-[45deg] -translate-y-2" : "rotate-[0deg] translate-y-0"}`}></span>
+                </div>
             </div>
+
+            <ul className={`w-screen h-[97%] fixed top-[4rem] ${toggleMenuNavbar ? "left-0" : "-left-[100%]"} md:hidden flex flex-col justify-center items-center bg-black opacity-90 transition-all duration-[1.1s] ease-out`}>
+                {
+                    NavItems.map(({ id, itemText, itemLink }) => (
+                        <li key={id} className="w-full my-3 text-center capitalize text-lg font-semibold">
+                            <a href={`#${itemLink}`} className="block py-4 transition-all duration-[0.7s] hover:text-primary">{itemText}</a>
+                        </li>
+                    ))
+                }
+            </ul>
         </nav >
     )
 }
